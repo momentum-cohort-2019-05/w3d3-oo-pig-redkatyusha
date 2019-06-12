47a41e4c69098bad3ce84d9920d384b5f66cacdf
Malyutka Kobanova <tankettedawn@gmail.com>
2019-06-12 13:18:27 -0400

    added some empty structure to pig.py

---
+++ pig.py
@@ -0,0 +1,22 @@
+class PlayGame:
+    """
+    Plays the game dice Pig with two players.
+    """
+
+    def __init__(self):
+        pass
+
+    def get_opponent(self):
+        pass
+
+    def start_game(self):
+        pass
+    
+class Scoreboard:
+    """
+    Tracks turn score, total score, and multi-game score.
+    """
+
+    def __init__(self, player_one, player_two)
+        self.player_one = player_one
+        self.player_two = player_two
