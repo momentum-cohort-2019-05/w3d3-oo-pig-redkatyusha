14e3069f5b13ed866629f49f4fa159d8a0eed9ba
Malyutka Kobanova <tankettedawn@gmail.com>
2019-06-12 23:45:45 -0400

    add more structural stuff

--- pig.py
+++ pig.py
@@ -3,12 +3,29 @@ class Game:
     Plays the dice game Pig with two players.
     """
 
+    def __init__(self):
+        pass
+
 class Scoreboard:
     """
     Keeps track of the turn score and total score.
     """
 
+    def __init__(self):
+        pass
+
 class Player:
     """
     Keeps track of the players in the game.
     """
+
+    def __init__(self):
+        pass
+
+class Die:
+    """
+    Roll a die!
+    """
+
+    def __init__(self):
+        pass
