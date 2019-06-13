c5babfeae3d4446bd83983229e013e03df6008c8
Malyutka Kobanova <tankettedawn@gmail.com>
2019-06-12 23:43:11 -0400

    added some empty classes

---
+++ pig.py
@@ -0,0 +1,14 @@
+class Game:
+    """
+    Plays the dice game Pig with two players.
+    """
+
+class Scoreboard:
+    """
+    Keeps track of the turn score and total score.
+    """
+
+class Player:
+    """
+    Keeps track of the players in the game.
+    """
